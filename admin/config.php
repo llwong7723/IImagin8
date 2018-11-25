01accd8c053bf61989e4f97ce18be57879e03a38
Kenji <wly.kenji@gmail.com>
2018-11-25 14:08:06 +0800

    Initial Commit
    adding and removing files

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/iimagin8/admin/');
+define('HTTP_CATALOG', 'http://localhost/iimagin8/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/iimagin8/admin/');
+define('HTTPS_CATALOG', 'http://localhost/iimagin8/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/iimagin8/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/iimagin8/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/iimagin8/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/iimagin8/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'iimagin8');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
